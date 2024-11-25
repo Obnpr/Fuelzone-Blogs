c3c119d3283f7f206fac3268304b3eeaef607db7
Obed Negron <oneg9029@interponce.edu>
2024-11-25 17:09:37 -0400

    Fix signup.aspx pagina

---
+++ Fuelzone/Fuelzone/pages/user_registration/Signup.aspx.designer.cs
@@ -0,0 +1,10 @@
+﻿namespace Fuelzone.pages.user_registration
+{
+    public partial class Signup
+    {
+        protected global::System.Web.UI.WebControls.TextBox txtUsername;
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+        protected global::System.Web.UI.WebControls.Label lblMessage;
+    }
+}
