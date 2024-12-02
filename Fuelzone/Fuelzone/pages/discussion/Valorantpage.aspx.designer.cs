613f28400a752190796c37d9930d4ade3560b23b
Obed Negron <oneg9029@interponce.edu>
2024-12-01 23:29:18 -0400

    Added darkmode, fix comments a little.

--- Fuelzone/Fuelzone/pages/discussion/Valorantpage.aspx.designer.cs
+++ Fuelzone/Fuelzone/pages/discussion/Valorantpage.aspx.designer.cs
@@ -15,6 +15,33 @@ namespace Fuelzone
     {
 
         /// <summary>
+        /// lblLoggedInAs control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLoggedInAs;
+
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
         /// commentInput control.
         /// </summary>
         /// <remarks>
@@ -31,6 +58,15 @@ namespace Fuelzone
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button SubmitCommentButton;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
 
         /// <summary>
         /// lblMessage control.
